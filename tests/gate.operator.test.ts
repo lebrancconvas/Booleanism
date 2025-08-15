b799bae83bbb9e5b8d04582b18f3dd4889a6ad7b
lebrancconvas <lebrancconvas@gmail.com>
2025-08-15 23:42:48 +0700

    ✅ test: update tests.

--- tests/gate.operator.test.ts
+++ tests/gate.operator.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "bun:test";
 import { NAND, NOR, XNOR, XOR } from "../utils/operators/gate";
+import { IFF } from "../utils/operators";
 
 describe("Logic Gate Operator Test.", () => {
   it("NAND Operator Test.", () => {
@@ -28,5 +29,12 @@ describe("Logic Gate Operator Test.", () => {
     expect(XNOR(true, false)).toBe(false);
     expect(XNOR(false, true)).toBe(false);
     expect(XNOR(false, false)).toBe(true);
+  });
+
+  it("XNOR is the same as IFF.", () => {
+    expect(XNOR(true, true)).toBe(IFF(true, true));
+    expect(XNOR(true, false)).toBe(IFF(true, false));
+    expect(XNOR(false, true)).toBe(IFF(false, true));
+    expect(XNOR(false, false)).toBe(IFF(false, false));
   })
 });

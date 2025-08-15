c7f105597edc053b72dfe701cbcc48a308fb9f69
lebrancconvas <lebrancconvas@gmail.com>
2025-08-15 23:33:31 +0700

    ✅ test: update tests.

--- tests/logic.test.ts
+++ tests/logic.test.ts
@@ -3,81 +3,87 @@ import { P } from "../utils/proposition";
 import { AND, NOT, OR, IMPLY, IFF } from "../utils/operators";
 
 describe("Logic Test", () => {
-  let p = new P(true, "This Sentence is True.");
-  let q = new P(false, "This Sentence is False.");
-  let r = new P(true, "This Sentence is Not False");
-  let s = new P(false, "This Sentence is Not True");
-
   it("~(~P) === P", () => {
-    expect(NOT(NOT(p.value))).toBe(p.value);
-    expect(NOT(NOT(q.value))).toBe(q.value);
+    expect(NOT(NOT(true))).toBe(true);
+    expect(NOT(NOT(false))).toBe(false);
   });
 
   it("P v ~P === T", () => {
-    expect(OR(p.value, NOT(p.value))).toBe(true);
-    expect(OR(q.value, NOT(q.value))).toBe(true);
+    expect(OR(true, NOT(true))).toBe(true);
+    expect(OR(false, NOT(false))).toBe(true);
   });
 
   it("P ^ ~P === F", () => {
-    expect(AND(p.value, NOT(p.value))).toBe(false);
-    expect(AND(q.value, NOT(p.value))).toBe(false);
+    expect(AND(true, NOT(true))).toBe(false);
+    expect(AND(false, NOT(false))).toBe(false);
   });
 
   it("P ^ P === P", () => {
-    expect(AND(p.value, p.value)).toBe(p.value);
-    expect(AND(q.value, q.value)).toBe(q.value);
+    expect(AND(true, true)).toBe(true);
+    expect(AND(false, false)).toBe(false);
   });
 
   it("P v P === P", () => {
-    expect(OR(p.value, p.value)).toBe(p.value);
-    expect(OR(q.value, q.value)).toBe(q.value);
+    expect(OR(true, true)).toBe(true);
+    expect(OR(false, false)).toBe(false);
   });
 
   it("P ^ F === F", () => {
-    expect(AND(p.value, false)).toBe(false);
-    expect(AND(q.value, false)).toBe(false);
+    expect(AND(true, false)).toBe(false);
+    expect(AND(false, false)).toBe(false);
   });
 
   it("P ^ T === P", () => {
-    expect(AND(p.value, true)).toBe(p.value);
-    expect(AND(q.value, true)).toBe(q.value)
+    expect(AND(true, true)).toBe(true);
+    expect(AND(false, true)).toBe(false);
   });
 
   it("P v T === T", () => {
-    expect(OR(p.value, true)).toBe(true);
-    expect(OR(q.value, true)).toBe(true);
+    expect(OR(true, true)).toBe(true);
+    expect(OR(false, true)).toBe(true);
   });
 
   it("P v F === P", () => {
-    expect(OR(p.value, false)).toBe(p.value);
-    expect(OR(q.value, false)).toBe(q.value);
+    expect(OR(true, false)).toBe(true);
+    expect(OR(false, false)).toBe(false);
   });
 
   it("(P -> Q) === (~Q -> ~P)", () => {
-    expect(IMPLY(p.value, q.value)).toBe(IMPLY(NOT(q.value), NOT(p.value)));
-    expect(IMPLY(q.value, p.value)).toBe(IMPLY(NOT(p.value), NOT(q.value)));
+    expect(IMPLY(true, false)).toBe(IMPLY(NOT(false), NOT(true)));
+    expect(IMPLY(false, true)).toBe(IMPLY(NOT(true), NOT(false)));
+    expect(IMPLY(true, true)).toBe(IMPLY(NOT(true), NOT(true)));
+    expect(IMPLY(false, false)).toBe(IMPLY(NOT(false), NOT(false)));
   });
 
   it("(P -> Q) === (~P v Q)", () => {
-    expect(IMPLY(p.value, q.value)).toBe(OR(NOT(p.value), q.value));
-    expect(IMPLY(q.value, p.value)).toBe(OR(NOT(q.value), p.value));
+    expect(IMPLY(true, false)).toBe(OR(NOT(true), false));
+    expect(IMPLY(false, true)).toBe(OR(NOT(false), true));
+    expect(IMPLY(true, true)).toBe(OR(NOT(true), true));
+    expect(IMPLY(false, false)).toBe(OR(NOT(false), false));
   })
 
   it("~(P ^ Q) === (~P v ~Q)", () => {
-    expect(NOT(AND(p.value, q.value))).toBe(OR(NOT(p.value), NOT(q.value)));
-    expect(NOT(AND(q.value, p.value))).toBe(OR(NOT(q.value), NOT(p.value)));
+    expect(NOT(AND(true, false))).toBe(OR(NOT(true), NOT(false)));
+    expect(NOT(AND(false, true))).toBe(OR(NOT(false), NOT(true)));
+    expect(NOT(AND(true, true))).toBe(OR(NOT(true), NOT(true)));
+    expect(NOT(AND(false, false))).toBe(OR(NOT(false), NOT(false)));
   });
 
   it("~(P v Q) === (~P ^ ~Q)", () => {
-    expect(NOT(OR(p.value, q.value))).toBe(AND(NOT(p.value), NOT(q.value)));
-    expect(NOT(OR(q.value, p.value))).toBe(AND(NOT(q.value), NOT(p.value)));
+    expect(NOT(OR(true, false))).toBe(AND(NOT(true), NOT(false)));
+    expect(NOT(OR(false, true))).toBe(AND(NOT(false), NOT(true)));
+    expect(NOT(OR(true, true))).toBe(AND(NOT(true), NOT(true)));
+    expect(NOT(OR(false, false))).toBe(AND(NOT(false), NOT(false)));
   });
 
   it("(P <-> Q) === (P -> Q) ^ (Q -> P)", () => {
-    expect(IFF(p.value, q.value))
-      .toBe(AND(IMPLY(p.value, q.value), IMPLY(q.value, p.value)));
-    expect(IFF(q.value, p.value))
-      .toBe(AND(IMPLY(q.value, p.value), IMPLY(p.value, q.value)));
+    expect(IFF(true, false))
+      .toBe(AND(IMPLY(true, false), IMPLY(false, true)));
+    expect(IFF(false, true))
+      .toBe(AND(IMPLY(false, true), IMPLY(true, false)));
+    expect(IFF(true, true))
+      .toBe(AND(IMPLY(true, true), IMPLY(true, true)));
+    expect(IFF(false, false))
+      .toBe(AND(IMPLY(false, false), IMPLY(false, false)));
   });
-
 });
